3fee3dd188b86b1da9d2d8a0d49ce55818609716
D0rianGrey <D0rianGrey@users.noreply.github.com>
2024-11-24 13:16:38 +0000

    deploy: 474ff7c12f0e366c08e1bb45303a437ed82f0e15

--- pages/modules/productsBar/ProductNameEnum.ts
+++
@@ -1,8 +0,0 @@
-export enum ProductName {
-    STAYS = 'Stays',
-    FLIGHTS = 'Flights',
-    CARS = 'Cars',
-    PACKAGES = 'Packages',
-    THINGS_TO_DO = 'Things to do',
-    CRUISES = 'Cruises',
-}
--- tests/ui/openMainPageWithCustomContex.test.ts
+++
@@ -1,9 +0,0 @@
-import {test} from '../../utils/testFixtures';
-
-
-test('Example Test', async ({mainPage}) => {
-    await mainPage.navigate();
-    // await mainPage.clickOnProduct(ProductName.FLIGHTS)
-    await mainPage.verifyThatStaysIsSelectedByDefault();
-    await mainPage.clickOnEachProduct();
-});
